18bd45b729d5d6d53afe5b2a62af4cb829d18165
Carlos Hernandez <102351215+CHRLWH@users.noreply.github.com>
2025-03-19 11:18:19 +0100

    Commit

---
+++ Comandos/ConectorComando.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace wpfMVVM_Prueba.Comandos
+{
+    public class ConectorComando : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        private Action<object> _Execute { get; set; }
+        private Predicate<object> _CanExecute { get; set; }
+
+        public ConectorComando(Action<object> ExecuteMethod, Predicate<object> CanExecuteMethod)
+        {
+            _Execute = ExecuteMethod;
+            _CanExecute = CanExecuteMethod;
+        }
+        public bool CanExecute(object? parameter)
+        {
+            return _CanExecute(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            _Execute(parameter);
+        }
+    }
+}
---
+++ Model/UserManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpfMVVM_Prueba.Model
+{
+    class UserManager
+    {
+        public static ObservableCollection<User> _UsuarioDeLaBaseDeDatos { get; set; } = new ObservableCollection<User>() { new User() { Email = "prueba@mail.com", Nombre = "Nombre" }, new User() { Email = "prueba2@mail.com", Nombre = "Nombre" } };
+
+        public static ObservableCollection<User> GetUsuarios()
+        {
+            return _UsuarioDeLaBaseDeDatos;
+
+        }
+
+        public static void AddUsers(User usuario)
+        {
+            _UsuarioDeLaBaseDeDatos.Add(usuario);
+
+        }
+
+    }
+}
